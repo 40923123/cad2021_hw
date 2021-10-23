a1c07bdd40ccaf543df251f9ebdbb4e88c6ed7c2
40923123 <40923123@gm.nfu.edu.tw>
2021-10-23 11:57:50 +0800

    wink teach mp4 update error

---
+++ downloads/2021.10.23/2021.10.23.js
@@ -0,0 +1,20 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 722, y: 875, width: 60, height: 24 }, target: 3 },
+    ],
+    "469": [
+      { type: "gotoframe", rect: { x: 1405, y: 688, width: 60, height: 24 }, target: 470 },
+    ],
+    "505": [
+      { type: "gotoframe", rect: { x: 808, y: 522, width: 60, height: 24 }, target: 506 },
+    ],
+    "578": [
+      { type: "gotoframe", rect: { x: 1019, y: 481, width: 60, height: 24 }, target: 579 },
+    ],
+  },
+};
