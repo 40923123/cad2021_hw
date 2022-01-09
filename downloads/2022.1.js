8dd2cbea575edaff1ba3857e832394bf2895d61d
40923123 <40923123@gm.nfu.edu.tw>
2022-01-10 00:58:32 +0800

    cad2021final by 6

---
+++ downloads/2022.1.js
@@ -0,0 +1,23 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 694, y: 151, width: 60, height: 24 }, target: 3 },
+    ],
+    "120": [
+      { type: "gotoframe", rect: { x: 971, y: 343, width: 60, height: 24 }, target: 121 },
+    ],
+    "151": [
+      { type: "gotoframe", rect: { x: 821, y: 569, width: 60, height: 24 }, target: 152 },
+    ],
+    "179": [
+      { type: "gotoframe", rect: { x: 720, y: 437, width: 60, height: 24 }, target: 180 },
+    ],
+    "223": [
+      { type: "gotoframe", rect: { x: 826, y: 91, width: 60, height: 24 }, target: 224 },
+    ],
+  },
+};
